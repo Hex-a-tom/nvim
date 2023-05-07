7f7c8c793f265cdda93566f8396539ecc8d30ca9
Felix Sondhauss <hexatom123@gmail.com>
2023-05-07 14:49:28 +0200

    Fix git error on commit

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -12,6 +12,7 @@ return {
 		-- TODO: Fix confirmation dialog
 		"TimUntersberger/neogit",
 		opts = {
+			disable_commit_confirmation = true,
 			integrations = {
 				-- Neogit only provides inline diffs. If you want a more traditional way to look at diffs, you can use `sindrets/diffview.nvim`.
 				-- The diffview integration enables the diff popup, which is a wrapper around `sindrets/diffview.nvim`.
